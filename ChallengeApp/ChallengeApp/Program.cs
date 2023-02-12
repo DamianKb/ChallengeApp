e1e89dd10fcbb2c99ac8f2cbcd5caf60c380bf39
Damian Kubsiak <damian.kubasiak@op.pl>
2023-02-12 21:18:34 +0100

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int myAge = 33;
+string myName = "Ewa";
+string mySex = "Kobieta";
+string mySex2 = "Mężczyzna";
+var myAge2 = 17;
+
+
+if (myAge < 40 && mySex == "Kobieta")
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+
+else
+{
+    Console.WriteLine("Nie jestem Kobiętą poniżej 30 lat");
+}
+
+if (myName == "Ewa" && myAge == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+
+if (mySex2 != "Kobieta" && myAge2 < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+
+if (myAge > 30 && mySex2 == "Mężczyzna")
+{
+    if (myAge < 40)
+        Console.WriteLine("Jestem mężczyzną w sile wieku");
+}
+
+
+
+
+
+
+
+

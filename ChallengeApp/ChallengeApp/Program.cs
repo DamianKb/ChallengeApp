e39f00447c6bfa9876913145d51e7337585394b9
Damian Kubsiak <damian.kubasiak@op.pl>
2023-02-14 22:47:23 +0100

    Zadanie dzień 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,43 +1,81 @@
-﻿int myAge = 33;
-string myName = "Ewa";
-string mySex = "Kobieta";
-string mySex2 = "Mężczyzna";
-var myAge2 = 17;
-bool man = true;
+﻿int number = 4556;
+string numberAsString = number.ToString();
+char[] letters = numberAsString.ToCharArray();
 
-if (myAge < 40 && mySex == "Kobieta")
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+
+
+
+
+foreach (char letter in letters)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
+   if(letter == '0')
+    { 
+        counter0++;
+    }
+    else if (letter == '1')
+    {
+        counter1++;
+    }
+    else if (letter == '2')
+    { 
+        counter2++; 
+    }
+   else if (letter == '3')
+    { 
+        counter3++; 
+    }
+    else if (letter == '4')
+    {
+        counter4++;
+    }
+    else if (letter == '5')
+    {
+        counter5++;
+    }
+    else if (letter == '6')
+    {
+        counter6++;
+    }
+    else if (letter == '7')
+    {
+        counter7++;
+    }
+    else if (letter == '8')
+    {
+        counter8++;
+    }
+    else if (letter == '9')
+    {
+        counter9++;
+    }
+     
 }
 
-else
 {
-    Console.WriteLine("Nie jestem Kobiętą poniżej 30 lat");
-}
+    Console.WriteLine("Wyniki dla liczby:" + number);
+    Console.WriteLine("0 => " + counter0);
+    Console.WriteLine("1 => " + counter1);
+    Console.WriteLine("2 => " + counter2);
+    Console.WriteLine("3 => " + counter3);
+    Console.WriteLine("4 => " + counter4);
+    Console.WriteLine("5 => " + counter5);
+    Console.WriteLine("6 => " + counter6);
+    Console.WriteLine("7 => " + counter7);
+    Console.WriteLine("8 => " + counter8);
+    Console.WriteLine("9 => " + counter9);
 
-if (myName == "Ewa" && myAge == 33)
-{
-    Console.WriteLine("Ewa, lat 33");
 }
-
-if (mySex2 != "Kobieta" && myAge2 < 18)
-{
-    Console.WriteLine("Niepełnoletni Mężczyzna");
-}
-
-if (myAge > 30 && mySex2 == "Mężczyzna")
-{
-    if (myAge < 40)
-        Console.WriteLine("Jestem mężczyzną w sile wieku");
-}
-
-if (man == true)
-{
-    Console.WriteLine("Jestem Mężczyzną");
-}
-if (man != false)
-    Console.WriteLine("Jestem Kobietą");
-
+    
 
 
 
